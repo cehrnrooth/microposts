56f223856eadbef258e79f97e9a0b4e34e0301b1
Carl Ehrnrooth <carl.ehrnrooth@gmail.com>
2012-04-07 00:49:09 -0700

    updated tests and layouts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,46 +2,46 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
   describe "Home page" do
     
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
     
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+    end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => "| Home")
     end
     
   end
   
     describe "Help page" do
-      
-      it "should have the content 'Help'" do
-        visit '/static_pages/help'
-        page.should have_content('Help')
-      end
-      
-      it "should have the right title" do
-        visit '/static_pages/help'
-        page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
-      end
-      
+      before { visit '/static_pages/help' }
+
+      it { page.should have_selector('h1',    text: 'Help') }
+      it { page.should have_selector('title', text: "#{base_title} | Help") }
     end
 
     describe "About page" do
-      
-      it "should have the content 'About'" do
-        visit '/static_pages/about'
-        page.should have_content('About Us')
-      end
-      
-      it "should have the right title" do
-        visit '/static_pages/about'
-        page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
-      end
-      
+      before { visit '/static_pages/about' }
+
+      it { page.should have_selector('h1',    text: 'About') }
+      it { page.should have_selector('title', text: "#{base_title} | About") }
+    end
+
+    describe "Contact page" do
+      before { visit '/static_pages/contact' }
+
+      it { page.should have_selector('h1',    text: 'Contact') }
+      it { page.should have_selector('title', text: "#{base_title} | Contact") }
     end
 
 end
